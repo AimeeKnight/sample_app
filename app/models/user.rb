04949afff3ecf53049f46b003e4b70abc87340d3
Aimee Knight <AimeeKnight83@gmail.com>
2013-05-29 15:12:21 -0400

    Add test to ensure emails are downcased before save, refactored user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	has_secure_password
 
-  before_save { |user| user.email = email.downcase } #forces uniqueness on the database level in case users submits in quick succession
+  before_save { email.downcase! } #forces uniqueness on the database level in case users submits in quick succession
 
   validates :name,  presence: true, length: { maximum: 50 } #Rails validates the presence of an attribute using the blank? method
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
