378cead2fa324dde570424f8e85620980fe258cf
Aimee Knight <AimeeKnight83@gmail.com>
2014-05-24 13:16:28 -0500

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,20 +6,20 @@
 # no regular words or you'll be exposed to dictionary attacks.
 
 # Rails 4 
-#require 'securerandom'
+# require 'securerandom'
 
-#def secure_token
-#  token_file = Rails.root.join('.secret')
-#  if File.exist?(token_file)
-#    # Use the existing token.
-#    File.read(token_file).chomp
-#  else
-#    # Generate a new token and store it in token_file.
-#    token = SecureRandom.hex(64)
-#    File.write(token_file, token)
-#    token
+# def secure_token
+#    token_file = Rails.root.join('.secret')
+#    if File.exist?(token_file)
+#      #  Use the existing token.
+#      File.read(token_file).chomp
+#    else
+#    #  Generate a new token and store it in token_file.
+#      token = SecureRandom.hex(64)
+#      File.write(token_file, token)
+#      token
+#    end
 #  end
-#end
 
 #SampleApp::Application.config.secret_key_base = secure_token
 
